8768e9c17f5ca18206fd2810307529a5f8d7b6f7
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2024-01-09 22:30:16 -0600

    Added Pause Menu functionality. Logic stored inside the GameManager for now. Added logic to pause, return to menu, and quit game.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private bool _isGameOver = false;
+    private bool _isGamePaused = false;
+    [SerializeField]
+    private GameObject _pauseMenu;
 
     private void Update()
     {
@@ -13,10 +16,54 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
+
+        if ( Input.GetKeyDown(KeyCode.Escape) ) {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        if (_isGamePaused == false)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
     }
 
     public void GameOver()
     {
         _isGameOver = true;
+    }
+
+    public void PauseGame()
+    {
+        _isGamePaused = true;
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        _pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        _isGamePaused = false;
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        _pauseMenu.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
